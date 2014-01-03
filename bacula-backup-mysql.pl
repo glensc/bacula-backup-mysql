d8d56609701c49896a10d8cc67ec244deb1e7cfb
Elan Ruusamäe <glen@delfi.ee>
2014-01-03 18:53:29 +0200

    version 0.7

--- bacula-backup-mysql.pl
+++ bacula-backup-mysql.pl
@@ -32,7 +32,7 @@ use File::Path qw(rmtree);
 use constant EX_UNAVAILABLE => 69;
 use constant EX_SOFTWARE => 70;
 
-our $VERSION = '0.7-dev';
+our $VERSION = '0.7';
 
 # path to Apache HTTPd-style config
 my $configfile = '/etc/bacula/backup-mysql.conf';
